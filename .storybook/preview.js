7d4ec3dafebb3944abe85342f17e23c6daa693fd
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-18 17:15:07 -0400

    add chakra button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react'
+import { addDecorator } from '@storybook/react';
 import Center from '../src/components/Center/Center';
+import { ThemeProvider, theme, CSSReset, Box } from '@chakra-ui/core';
 
 // global decorator
-addDecorator(story => <Center>{story()}</Center>)
+addDecorator(story => (
+  <ThemeProvider theme={theme}>
+    <CSSReset />
+   <Box m='4'>{story()}</Box>
+  </ThemeProvider>
+ ))
 
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  options: {
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-  },
-}
+
