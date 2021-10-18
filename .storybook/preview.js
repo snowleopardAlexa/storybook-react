0e798b7abcc1ebc2bf4c9b3939da4d5ec39b324f
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-18 17:22:34 -0400

    add decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
-import Center from '../src/components/Center/Center';
 import { ThemeProvider, theme, CSSReset, Box } from '@chakra-ui/core';
 
-// global decorator
-addDecorator(story => (
-  <ThemeProvider theme={theme}>
-    <CSSReset />
-   <Box m='4'>{story()}</Box>
-  </ThemeProvider>
- ))
+// .storybook/preview.js
+
+export const parameters = {
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Box m="4">
+        <Story />
+      </Box>
+    </ThemeProvider>
+  ),
+];
 
 
