b792e9f8d9d1abedc9b1a7e110a8e2e94beb846f
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-18 16:26:45 -0400

    add center component, no need for center.stories because it is aligning component

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,6 +1,7 @@
 // writing button story
 import React from 'react';
 import Button from './Button';
+import Center from '../Center/Center';
 
 export default {
     title: 'Form/Button',
@@ -10,10 +11,10 @@ export default {
     }
 }
 
-export const Primary = () => <Button variant='primary'>Primary</Button>
-export const Secondary = () => <Button variant='secondary'>Secondary</Button>
-export const Success = () => <Button variant='success'>Success</Button>
-export const Danger = () => <Button variant='danger'>Danger</Button>
+export const Primary = () => <Center><Button variant='primary'>Primary</Button></Center>
+export const Secondary = () => <Center><Button variant='secondary'>Secondary</Button></Center>
+export const Success = () => <Center><Button variant='success'>Success</Button></Center>
+export const Danger = () => <Center><Button variant='danger'>Danger</Button></Center>
 
 
 const Template = args => <Button {...args} />
