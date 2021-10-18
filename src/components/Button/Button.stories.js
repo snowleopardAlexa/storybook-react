5eef87768bf06d3b49ab193e83117fdf22590a7f
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-18 16:33:26 -0400

    add decorators, remove Center, buttons still in the middle

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -6,34 +6,13 @@ import Center from '../Center/Center';
 export default {
     title: 'Form/Button',
     component: Button,
-    args: {
-        children: 'Button'
-    }
+    decorators: [story => <Center>{story()}</Center>]
 }
 
-export const Primary = () => <Center><Button variant='primary'>Primary</Button></Center>
-export const Secondary = () => <Center><Button variant='secondary'>Secondary</Button></Center>
-export const Success = () => <Center><Button variant='success'>Success</Button></Center>
-export const Danger = () => <Center><Button variant='danger'>Danger</Button></Center>
+export const Primary = () => <Button variant='primary'>Primary</Button>
+export const Secondary = () => <Button variant='secondary'>Secondary</Button>
+export const Success = () => <Button variant='success'>Success</Button>
+export const Danger = () => <Button variant='danger'>Danger</Button>
 
 
-const Template = args => <Button {...args} />
-
-export const PrimaryA = Template.bind({ })
-PrimaryA.args = {
-    variant: 'primary',
-    // children: 'Primary Args'
-}
-
-export const LongPrimaryA = Template.bind({ })
-LongPrimaryA.args = {
-    ...PrimaryA.args,
-    // children: 'Long Primary Args'
-}
-
-export const SecondaryA = Template.bind({ })
-SecondaryA.args = {
-    variant: 'secondary',
-    // children: 'Secondary Args'
-}
 
