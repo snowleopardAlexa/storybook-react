22a7878771ee2b4abd331de280fcb54aed9b1eff
snowleopardAlexa <alexaslomski@gmail.com>
2021-10-18 20:15:16 -0400

    add log btn

--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -7,7 +7,21 @@ export default {
     component: Button
 }
 
-export const Success = () => <Button onClick={action('Click handler')} variantColor='green'>Success</Button>
+export const Success = () => (
+   <Button onClick={action('Click handler')} variantColor='green'>
+       Success
+   </Button>
+)
 
-export const Danger = () => <Button {...actions('onClick', 'onMouseOver')} variantColor='red'>Danger</Button>
+export const Danger = () => (
+   <Button {...actions('onClick', 'onMouseOver')} variantColor='red'>
+       Danger
+   </Button>
+)
+
+export const log = () => (
+    <Button variantColor='blue' onClick={() => console.log('Button clicked')}>
+        Log
+    </Button>
+)
 
