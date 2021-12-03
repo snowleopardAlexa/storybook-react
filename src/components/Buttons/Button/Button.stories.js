3f04cea7f2bcc9a55c0308451bca8654c376017f
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-03 17:59:29 -0500

    come back to the beggining

---
+++ src/components/Buttons/Button/Button.stories.js
@@ -0,0 +1,20 @@
+// writing button story
+import React from 'react';
+import Button from './Button';
+
+export default {
+    title: 'Form/Button',
+    component: Button,
+}
+
+export const Aqua = () => <Button variant='aqua'>Aqua</Button>
+export const Mandarine = () => <Button variant='mandarine'>Mandarine</Button>
+export const Barbie = () => <Button variant='barbie'>Barbie</Button>
+export const Amethyst = () => <Button variant='amethyst'>Amethyst</Button>
+
+
+
+
+
+
+
