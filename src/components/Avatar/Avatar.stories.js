ecd71b90099f205f5b80326a1d6cf6d6fe597e06
snowleopardAlexa <alexaslomski@gmail.com>
2021-12-03 20:20:41 -0500

    finish avatar

--- src/components/Avatar/Avatar.stories.js
+++ src/components/Avatar/Avatar.stories.js
@@ -7,9 +7,9 @@ export default {
     component: Avatar,
 }
 
-export const Aqua = () => <Avatar variant='aqua-avatar' size='small'></Avatar>
-export const Mandarine = () => <Avatar variant='mandarine-avatar' size='medium'></Avatar>
-export const Barbie = () => <Avatar variant='barbie-avatar' size='large'></Avatar>
-export const Amethyst = () => <Avatar variant='amethyst-avatar'></Avatar>
-export const Eucalyptus = () => <Avatar variant='eucalyptus-avatar'></Avatar>
-export const Bay = () => <Avatar variant='bay-avatar'></Avatar>
+export const Aqua = () => <Avatar variant='aqua-avatar' size='small-avatar'></Avatar>
+export const Mandarine = () => <Avatar variant='mandarine-avatar' size='medium-avatar'></Avatar>
+export const Barbie = () => <Avatar variant='barbie-avatar' size='large-avatar'></Avatar>
+export const Amethyst = () => <Avatar variant='amethyst-avatar' size='medium-avatar'></Avatar>
+export const Eucalyptus = () => <Avatar variant='eucalyptus-avatar' size='medium-avatar'></Avatar>
+export const Bay = () => <Avatar variant='bay-avatar' size='medium-avatar'></Avatar>
